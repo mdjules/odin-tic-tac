96eb8722429f1cbd5d236d1e2dfa50f6b92b9a15
Michael Juliano <mdjules@gmail.com>
2025-07-13 13:51:56 -0400

    Restructed the console game
    checkGameBoard is broken into 5 returnable object properties
    (board, displayGameboard, updateGameboard, checkWinner, and isBoardFull)

--- tic-tac.js
+++ tic-tac.js
@@ -1,4 +1,4 @@
-const game = createGameboard()
+const game = createGameboard();
 
 function createGameboard() {
     const rows = 3;
@@ -13,17 +13,67 @@ function createGameboard() {
         board.push(row)
     }
 
-    return {rows, columns, board}
+    function displayGameboard() {
+        console.clear();
+        board.forEach(row => console.log(row.join(' | ')))
+        
+    }
+
+    function updateGameboard(x, y, symbol) {
+        if (board[x][y] === "") {
+            board[x][y] = symbol
+            return true
+        } else {
+            console.log("Cell already taken. Pick another.")
+            return false
+        };
+
+    };
+
+    function checkWinner(symbol) { 
+    for (let i = 0; i < 3; i++) {
+        if (board[i].every(cell => cell === symbol)) return true
+        if (board.map(row => row[i] === symbol)) return true
+    }
+
+    if ([1, 2, 3].every(i => board[i][i] === symbol)) return true
+    if ([1, 2, 3].every(i => board[i][2-i] === symbol)) return true
+
+    return false
+}
+
+    function isBoardFull() {
+        return board.flat().every(cell => cell != "")
+    }
+
+    return {board, displayGameboard, updateGameboard, checkWinner, isBoardFull}
 };
 createGameboard()
 
-function displayGameboard() {
-    console.log(game.board)
+function createPlayers(name, symbol) {
+    return {name, symbol};
 };
 
-function playerInput() {
-    const placementX = parseInt(prompt("Choose row index where you want to place an X or O:"));
-    const placementY = parseInt(prompt("Choose column index where you want to place an X or O:"));
-    const choice = prompt("Choose X or O:");
-    game.board[placementX][placementY] = choice;
+
+
+function gameLoop() {
+    const playerOne = createPlayers("Player One", "X");
+    const playerTwo = createPlayers("Player Two", "O");
+    let currentPlayer = playerOne;
+
+    while(true) {
+        game.displayGameboard();
+        console.log(`${currentPlayer.name}'s turn to place an ${currentPlayer.symbol}.`)
+        const x = parseInt(prompt("Select a row (0-2):"))
+        const y = parseInt(prompt("Select a column (0-2):"))
+
+        if (isNaN(x) || isNaN(y) || x > 2 || y > 2 || x < 0 || y < 0) {
+            console.log("Pick a valid cell.");
+            continue
+        };
+
+        const confirmedMove = game.updateGameboard(x, y, currentPlayer.symbol);
+        if (!confirmedMove) continue
+    }
 }
+
